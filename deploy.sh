231f5cd25dff73194a3c3ccc6e8dd4dd856e1143
Sergei Sobolev <sobolevsv@gmail.com>
2019-01-07 23:46:47 +0300

    test travis build

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 docker build -t sobolevsv/multi-client:latest -t sobolevsv/multi-client:$SHA -f ./client/Dockerfile ./client
 docker build -t sobolevsv/multi-server:latest -t sobolevsv/multi-server:$SHA -f ./server/Dockerfile ./server
 docker build -t sobolevsv/multi-worker:latest -t sobolevsv/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+
 docker push sobolevsv/multi-client:latest
 docker push sobolevsv/multi-server:latest
 docker push sobolevsv/multi-worker:latest
